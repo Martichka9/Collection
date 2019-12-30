97117a5345f70747dd6b725b7c6de8f7b30205ea
Martichka9 <Martichka9@users.noreply.github.com>
2019-12-30 02:22:42 +0200

    moved to new repo

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+"use strict";
+
+let gulp = require("gulp"),
+    csso = require("gulp-csso"),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	cp = require("child_process");
+
+gulp.task("sass", function() {
+	return gulp.src( '_scss/**/*.scss')
+		.pipe( sass().on('error', sass.logError) )
+		// .pipe( autoprefixer() )
+		.pipe( csso() )
+		.pipe( gulp.dest( './docs/css/' ) )
+		.pipe( browserSync.stream({ match: '**/*.css' }) )
+	;
+});
+
+gulp.task('copy', function() {
+    return gulp.src([
+		'_scss/fa/css/*.css',
+		'_scss/fa/fa/webfonts/*.eot',
+		'_scss/fa/webfonts/*.svg',
+		'_scss/fa/webfonts/*.ttf',
+		'_scss/fa/webfonts/*.woff',
+		'_scss/fa/webfonts/*.woff2',
+		'_scss/theme/*.css'
+	], {base:'_scss/'})
+        .pipe(gulp.dest('./docs/css/'));
+});
+
+// Jekyll
+gulp.task("jekyll-dev", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl ''"], { stdio: "inherit", shell: true });
+});
+
+// Jekyll
+gulp.task("jekyll", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		server: {
+            baseDir: "./docs/"
+		}
+	});
+
+	gulp.watch( '_scss/**/*.scss', gulp.series('sass') );
+
+	gulp.watch(
+		[
+			"./*.html",
+			"./*.yml",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_data/*.json"
+		]
+	).on('change', gulp.series('jekyll-dev', 'sass', 'copy') );
+	//).on('change', gulp.series('jekyll-dev', 'sass') );
+
+	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
+	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
+});
+
+gulp.task("default", gulp.series('jekyll-dev', 'sass', 'copy', 'watch'));
+//gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
+
+gulp.task("deploy", gulp.series('jekyll', 'sass', 'copy' , function() {
+//gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
+	return cp.spawn('git status && git commit -am "Update docs folder for GHPages, img alt + page lang fixes, " && git pull && git push', { stdio: "inherit", shell: true });
+}));
