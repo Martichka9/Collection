9117403ad4d1f13203e3eee12efeb506c6b2f34e
Martichka9 <Martichka9@users.noreply.github.com>
2020-03-05 19:57:50 +0200

    fix nav links

--- gulpfile.js
+++ gulpfile.js
@@ -69,5 +69,5 @@ gulp.task("default", gulp.series('jekyll-dev', 'sass', 'copy', 'watch'));
 
 gulp.task("deploy", gulp.series('jekyll', 'sass', 'copy' , function() {
 //gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "add new link, fix About text, update footer links" && git pull && git push', { stdio: "inherit", shell: true });
+	return cp.spawn('git status && git commit -am "fix nav links" && git pull && git push', { stdio: "inherit", shell: true });
 }));
